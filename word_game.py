55b5d33e006b1f828fc54fd0f2bda17efa0f6d8a
Ghanshyam Agrawal <32092151+ghanshyam1898@users.noreply.github.com>
2020-10-29 10:46:07 +0530

    better code

--- word_game.py
+++ word_game.py
@@ -1,20 +1,22 @@
-if __name__ == '__main__':
-    with open("DICTIONORY", 'r') as myfile:
-        data = myfile.read()
-        words = data.split("\n")
-    told = []
+with open("DICTIONORY", 'r') as myfile:
+    data = myfile.read()
+    words = data.split("\n")
+
+told = []
+word = 'rgbsfvsdc'
+
+while True:
+    # receive a new valid word from user
+    while word not in words and word not in told:
+        word = raw_input("Enter your word : ")
+
+    for item in words:
+        # say the word ending with alphabet as the last alphabet of word user has told
+        if item[0] == word[len(word)-1] and item not in told:
+            # ignore words ending with 's and choose word with at least 4 alphabets
+            if len(item) > 3 and not item.endswith("'s"):
+                told.append(item)
+                told.append(word)
+                print "My word is " + item + "\n\n"
+                break
     word = 'rgbsfvsdc'
-    
-    while True:
-        while word not in words and word not in told:
-            word = raw_input("Enter your word : ")
-        
-        for item in words:
-            if item[0] == word[len(word)-1] and item not in told:
-                if len(item) > 3 and "'" not in item:
-                    told.append(item)
-                    told.append(word)
-                    print "My word is " + item
-                    print "\n\n"
-                    break
-        word = 'rgbsfvsdc'
